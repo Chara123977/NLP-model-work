d820442176fbd9baf07659d0afd76886fc361b75
Chara123977 <146728957+Chara123977@users.noreply.github.com>
2025-12-28 22:50:29 +0800

    third commit

--- config/chip_config.py
+++ config/chip_config.py
@@ -6,12 +6,12 @@ class Config:
         self.data_dir = "datasets/CHIP-CTC"  # 你的数据路径
         self.model_name = "ernie-3.0-base-zh"
         self.batch_size = 32
-        self.epochs = 5
+        self.epochs = 10
         self.init_lr = 3e-5
         self.max_seq_len = 128
-        self.lr_step_size = 2
-        self.lr_gamma = 0.8
-        self.patience = 4
+        self.lr_step_size = 3
+        self.lr_gamma = 0.9
+        self.patience = 5
         self.min_delta = 0.001
         self.save_dir = "runs"
         self.device = paddle.set_device("gpu" if paddle.is_compiled_with_cuda() else "cpu")
